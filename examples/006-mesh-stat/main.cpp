7049a5b78afb729aebb4896534f15144b31fb7ce
alemuntoni <alemuntoni@users.noreply.github.com>
2024-04-19 13:42:18 +0200

    enable example 006

--- examples/006-mesh-stat/main.cpp
+++ examples/006-mesh-stat/main.cpp
@@ -22,9 +22,14 @@
 
 #include <iostream>
 
+#ifndef VCLIB_WITH_MODULES
 #include <vclib/algorithms.h>
 #include <vclib/load_save.h>
 #include <vclib/meshes.h>
+#else
+#include <Eigen/Core>
+import vclib;
+#endif
 
 int main()
 {
