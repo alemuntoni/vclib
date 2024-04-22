c4a4f2823c18bfe3e1473279a52bd8428bf6afbb
Alessandro Muntoni <alemuntoni@users.noreply.github.com>
2024-04-22 11:32:19 +0200

    add include in module for element mesh concepts scripts

--- scripts/generators/el_mesh_concept.py
+++ scripts/generators/el_mesh_concept.py
@@ -2,9 +2,12 @@ from . import common
 
 def generate_elem_mesh_concepts(element):
     include_file = "mesh/" + element.name + '_mesh_concept.h'
-    target_file = "include/vclib/concepts/" + include_file
+    module_include_file = "vclib/concepts/" + include_file
+    target_file = "include/" + module_include_file
     template_file = "concepts/mesh/element_mesh_concept.h"
 
     common.insert_include_in_file("include/vclib/concepts/mesh.h", include_file)
+
+    common.insert_include_in_file("src/vclib/concepts/mesh/element_mesh_concept.ixx", module_include_file, True)
 
     return common.simple_string_replacement(target_file, template_file, element)
