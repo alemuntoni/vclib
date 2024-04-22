4cb9c36f40d47155238ce1e1cbd9462e570d80b0
Alessandro Muntoni <alemuntoni@users.noreply.github.com>
2024-04-22 12:20:38 +0200

    add include in module for element views scripts

--- scripts/generators/el_view.py
+++ scripts/generators/el_view.py
@@ -2,9 +2,12 @@ from . import common
 
 def generate_elem_view(element):    
     include_file = "elements/" + element.name + '.h'
-    target_file = "include/vclib/views/mesh/" + include_file
+    module_include_file = "vclib/views/mesh/" + include_file
+    target_file = "include/" + module_include_file
     template_file = "views/mesh/element.h"
 
     common.insert_include_in_file("include/vclib/views/mesh/elements.h", include_file)
+
+    common.insert_include_in_file("src/vclib/views/mesh/elements.ixx", module_include_file, True)
 
     return common.simple_string_replacement(target_file, template_file, element)
