16102b1b543043b32dd30231fa55cdde4c2ab0be
Alessandro Muntoni <alemuntoni@users.noreply.github.com>
2024-04-22 11:40:29 +0200

    add include in module for element range concept scripts

--- scripts/generators/el_range_concept.py
+++ scripts/generators/el_range_concept.py
@@ -2,9 +2,12 @@ from . import common
 
 def generate_elem_range_concept(element):
     include_file = "mesh/" + element.name + '_range.h'
-    target_file = "include/vclib/concepts/ranges/mesh/" + element.name + '_range.h'
+    module_include_file = "vclib/concepts/ranges/mesh/" + element.name + '_range.h'
+    target_file = "include/" + module_include_file
     template_file = "concepts/ranges/mesh/element_range.h"
 
     common.insert_include_in_file("include/vclib/concepts/ranges/mesh.h", include_file)
+
+    common.insert_include_in_file("src/vclib/concepts/ranges/mesh.ixx", module_include_file, True)
 
     return common.simple_string_replacement(target_file, template_file, element)
