2f3589e5f53fea101cea9b18390722d054cd7fb8
alemuntoni <alemuntoni@users.noreply.github.com>
2024-07-24 10:25:28 +0200

    [types] move uint and ushort definitions inside vcl namespace

--- include/vclib/types/base.h
+++ include/vclib/types/base.h
@@ -27,12 +27,11 @@
 #include <concepts>
 #include <limits>
 #include <numeric>
+namespace vcl {
 
 // Define some basic types, for convenience
 using uint   = unsigned int;
 using ushort = unsigned short;
-
-namespace vcl {
 
 /**
  * @brief The UINT_NULL value represent a null value of uint that is the maximum
