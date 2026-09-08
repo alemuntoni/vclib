89deec7a0b4bc0270a244a92a36d11110ea175c3
alemuntoni <alemuntoni@users.noreply.github.com>
2026-09-08 15:47:05 +0200

    [ci] add workflow for installer standalone examples

--- 3rdparty/bgfx.cmake
+++ 3rdparty/bgfx.cmake
@@ -143,4 +143,18 @@ if(TARGET vclib-3rd-bgfx)
         FILES ${CMAKE_CURRENT_SOURCE_DIR}/cmake/vclib_shader_combinations.cmake
         DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/vclib
     )
+
+    if(UNIX AND NOT APPLE)
+        # bgfx.cmake installs a prebuilt x86-64 libdxcompiler.so on Linux into bin/.
+        # On non-x86_64 architectures (e.g. ARM64) this is a foreign-architecture binary
+        # that breaks dpkg-shlibdeps during DEB packaging. Furthermore, DirectX shader
+        # compilation is not used on Linux by VCLib.
+        install(CODE [[
+            file(GLOB_RECURSE _DXCOMPILER_FILES "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/*libdxcompiler.so*")
+            foreach(_F IN LISTS _DXCOMPILER_FILES)
+                message(STATUS "Removing unneeded DirectX shader compiler library: ${_F}")
+                file(REMOVE "${_F}")
+            endforeach()
+        ]])
+    endif()
 endif()
