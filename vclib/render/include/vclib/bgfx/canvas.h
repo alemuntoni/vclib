2644bac62430afb2714eaa59100ed9a07963072f
alemuntoni <alemuntoni@users.noreply.github.com>
2026-09-11 10:26:34 +0200

    [render] fix screenshot with multiplier

--- vclib/render/include/vclib/bgfx/canvas.h
+++ vclib/render/include/vclib/bgfx/canvas.h
@@ -176,22 +176,16 @@ public:
 
         if (newReadRequested) {
             // ONLY draw the offscreen frame
-            offscreenFrame();
+            const bool solicit = offscreenFrame();
 
-            // submit the calls for blitting the offscreen depth buffer BEFORE
-            // frame() so it happens in the same execution pass
-            // (mViewOffscreenId executes last)
-            const bool solicit = mReadRequest->submit();
-
-            mCurrFrame = bgfx::frame();
-
-            // Restore view state for the main view now that the frame has been
-            // submitted
             bgfx::setViewClear(
                 mViewId,
                 BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH | BGFX_CLEAR_STENCIL,
                 mDefaultClearColor.rgba());
             bgfx::setViewFrameBuffer(mViewId, mFbh);
+            bgfx::setViewRect(mViewId, 0, 0, mSize.x(), mSize.y());
+            DerivedRenderApp::CNV::resizeDrawers(
+                derived(), mSize.x(), mSize.y());
 
             if (solicit) {
                 // solicit new frame
@@ -355,9 +349,12 @@ public:
 
 private:
     // draw offscreen frame
-    void offscreenFrame()
+    bool offscreenFrame()
     {
         assert(mReadRequest != std::nullopt && mReadRequest->isPending());
+
+        auto originalSize = mSize;
+        auto originalFbh  = mFbh;
 
         // Disable clear on the read request view, since its ID is higher and
         // it executes after additional views. We use mViewId to clear instead.
@@ -366,8 +363,10 @@ private:
             mReadRequest->viewId(), mReadRequest->frameBuffer());
         bgfx::touch(mReadRequest->viewId());
 
-        auto tmpFbh = mFbh;
-        mFbh        = mReadRequest->frameBuffer();
+        mSize = mReadRequest->size();
+        DerivedRenderApp::CNV::resizeDrawers(derived(), mSize.x(), mSize.y());
+
+        mFbh = mReadRequest->frameBuffer();
 
         bgfx::setViewFrameBuffer(mViewId, mFbh);
 
@@ -379,6 +378,8 @@ private:
             mViewId,
             BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH | BGFX_CLEAR_STENCIL,
             clearValue);
+
+        bgfx::setViewRect(mViewId, 0, 0, mSize.x(), mSize.y());
 
         bgfx::touch(mViewId);
 
@@ -393,7 +394,19 @@ private:
         default: assert(false && "unsupported readback type"); break;
         }
 
-        mFbh = tmpFbh;
+        // submit the calls for blitting the offscreen depth buffer BEFORE
+        // frame() so it happens in the same execution pass
+        // (mViewOffscreenId executes last)
+        const bool solecit = mReadRequest->submit();
+
+        mCurrFrame = bgfx::frame();
+
+        // Restore view state for the main view now that the frame has been
+        // submitted
+        mFbh  = originalFbh;
+        mSize = originalSize;
+
+        return solecit;
     }
 
     auto* derived() { return static_cast<DerivedRenderApp*>(this); }
